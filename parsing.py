019a161f45a224d0623ae0e39183a48587e21264
Kirill Belyakov <dog.kirill.belyakov@gmail.com>
2021-12-11 21:19:30 +0300

    fix: web element classes up to date

--- parsing.py
+++ parsing.py
@@ -39,14 +39,14 @@ def create_driver(city):
 def choose_appartment_layout(driver, cian_link):
     driver.get(cian_link)
     # open a combobox
-    driver.find_elements_by_class_name("_025a50318d--button--2oXjq")[1].click()
+    driver.find_elements_by_class_name("_025a50318d--button--A0jv8")[1].click()
 
     # rooms 3 4 5 6
-    rooms = driver.find_elements_by_class_name('_025a50318d--button--39rK7')
+    rooms = driver.find_elements_by_class_name('_025a50318d--button--i1_mM')
     for room in rooms[2:]:
         room.click()
     # Studio apartment & free layout
-    extended_rooms = driver.find_elements_by_class_name('_025a50318d--box--VTgYk')
+    extended_rooms = driver.find_elements_by_class_name('_025a50318d--box--Sr_sA')
     for room in extended_rooms:
         room.click()
 
@@ -64,13 +64,16 @@ def download_files(data, city):
             search.send_keys(f"{row.raion_name}, {row.okrug}, {cities[city]}")
         else:
             search.send_keys(f"{row.raion_name}, {cities[city]}")
-        WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.CLASS_NAME, "_025a50318d--popper--c63zv")))
+        WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.CLASS_NAME, "_025a50318d--suggestion-list--NRzbC")))
+        
         search.send_keys(Keys.RETURN)
         driver.find_element_by_link_text('Найти').click()
         try:
             WebDriverWait(driver, 30).until(EC.url_contains("cat"))
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            driver.find_element_by_class_name("_93444fe79c--main--1-d6V").click()
+            driver.find_element_by_class_name("_93444fe79c--main--PpO9F").click()
+            # driver.find_elements_by_class_name("_93444fe79c--buttonGroup--Lf9rW").click()
+
         except:
             print(i, f"{row.raion_name}, {row.okrug}, Москва")
 
